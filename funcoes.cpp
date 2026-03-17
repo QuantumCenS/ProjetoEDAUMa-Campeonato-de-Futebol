5f6ed5fa0aafb7ff616872a8aae0bc176fea7608
CarlosS44 <2171325@student.uma.pt>
2026-03-17 17:19:50 +0000

    Update funcoes.cpp
    Criaçao da lista de transferencias e da lógica da adição de jogadores à mesma, funçao ordenar jogadores , e funçao preencher atributos independentes

--- funcoes.cpp
+++ funcoes.cpp
@@ -109,3 +109,78 @@ void libertarMemoria(Plantel& p, string* listaNomes) {
     delete[] p.jogadores;
     delete[] listaNomes;
 }
+
+
+
+void listaTranf(Equipa& e) {
+    e.capacidadeLT = 100;
+    e.totalLT = 0;
+    e.ListaTransf = new Jogador[e.capacidadeLT];                 //array duinamico do tipo jogador 
+
+
+}
+
+
+void adicionarJogLT(Equipa& e, string* listaNomes, int totalNomes) {
+
+    for (int i = 0; i < 2; i++) {   //  2 jogadores por jornada
+        if (e.totalLT < e.capacidadeLT) {
+
+            Jogador& novo = e.ListaTransf[e.totalLT];                              // 2. Criar um atalho para a primeira posição vazia no fim da lista
+            preencherAtributosIndependentes(novo,listaNomes,totalNomes);
+            Posicao posicoes[] = { GR, DEF, MED, AVA };
+            novo.pos = posicoes[gerarAleatorio(0, 3)];
+            novo.numero = gerarAleatorio(1, 99);
+
+            e.totalLT++;
+        }
+    }
+    OrdenarJogadores(e.ListaTransf, e.totalLT);
+
+}
+
+void exibirListaTransf(const Equipa& e) {
+    const char* posTxt[] = {"GR", "DEF", "MED", "AVA"};
+
+    cout << "************************************************************************\n";
+    cout<< "Lista de Transferencias:\n";
+    cout << "------------------------------------------------------------------------\n";
+
+    cout << "NUM | POS | IDADE | QUAL | LESAO | CAST | NOME\n";
+
+    for (int i = 0; i < e.totalLT; i++) {
+        Jogador& j = e.ListaTransf[i];
+        printf("%-3d | %-3s | %-5d | %-4d | %-4d%% | %-3d%% | %s\n",
+               j.numero, posTxt[j.pos], j.idade, j.qualidade,
+               j.probLesao, j.probCastigo, j.nome.c_str());
+    }
+    cout << "========================================================================\n";
+}
+
+
+
+// Numero e Posicao dependem das restricoes do plantel
+void preencherAtributosIndependentes(Jogador& novo, string* listaNomes, int totalNomes) {
+    novo.nome = listaNomes[gerarAleatorio(0, totalNomes - 1)];
+    novo.idade = gerarAleatorio(17, 38);
+    novo.probLesao = gerarAleatorio(0, 15);
+    novo.probCastigo = gerarAleatorio(0, 20);
+    novo.qualidade = gerarAleatorio(0, 100);
+}
+
+
+
+void OrdenarJogadores(Jogador* lista, int total) {
+
+    for (int i = 0; i < total - 1; i++) {
+        for (int j = 0; j < total - i - 1; j++) {
+
+            if (lista[j].pos > lista[j+1].pos) {
+
+                Jogador temporario = lista[j];
+                lista[j] = lista[j+1];
+                lista[j+1] = temporario;
+            }
+        }
+    }
+}
